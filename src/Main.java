4db4bf02ee81f1839a64fb1bd21bd0fe26fcd9ca
Michal Pavlovsky <mpavlvosky@gmail.com>
2023-04-10 23:17:18 +0200

    Pridany enum, abstract, metody move

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MapCreator[][] map = new MapCreator().createMap(3,5);
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[i].length; j++) {
-                System.out.print(map[i][j].toString());
-            }
-            System.out.println();
-        }
+        Game.game(3, 5);
 
     }
 }
--- src/MapCreator.java
+++ src/MapCreator.java
@@ -1,52 +1,21 @@
 public class MapCreator {
-    public MapCreator[][] createMap(int rows, int  columns) {
-        MapCreator[][] map = new MapCreator[rows][columns];
+    public LevelObject[][] createMap(int rows, int columns) {
+        LevelObject[][] map = new LevelObject[rows][columns];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                if (i == 0 || i==1 && j==0 || j == 4 || i == 2) {
-                    map[i][j] = new Wall();
-                } else if (i == 1 && j == 1){
-                    map[i][j] = new Man();
-                } else if (i==rows - 2 && j==columns-2) {
-                    map[i][j]= new Goal();
-                }else if (i==1 && j==2 ) {
-                    map[i][j] = new Box();
+                if (i == 0 || i == 1 && j == 0 || j == 4 || i == 2) {
+                    map[i][j] = new LevelObject.Wall();
+                } else if (i == 1 && j == 1) {
+                    map[i][j] = new LevelObject.Man();
+                } else if (i == rows - 2 && j == columns - 2) {
+                    map[i][j] = new LevelObject.Goal();
+                } else if (i == 1 && j == 2) {
+                    map[i][j] = new LevelObject.Box();
                 } else {
-                    map[i][j]=new Empty();
+                    map[i][j] = new LevelObject.Empty();
                 }
             }
         }
         return map;
     }
-
-public class Wall extends MapCreator{
-    public String toString() {
-
-    return "#";
-}}
-public class Empty extends MapCreator{
-        public String toString(){
-            return " ";
-        }
-}
-public class Man extends MapCreator {
-    public String toString() {
-        return "@";
-    }
-}
-public class Box extends MapCreator{
-    public String toString(){
-        return "$";
-    }
-}
-public class Goal extends MapCreator{
-    public String toString(){
-        return ".";
-    }
-}
-public class ReachGoal extends MapCreator{
-    public String toString(){
-        return ";";
-    }
-}
 }
