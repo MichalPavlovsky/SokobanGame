c8a14ba9a4b5288c0f0157ed542bbd3d44dcc7c1
Michal Pavlovsky <mpavlvosky@gmail.com>
2023-04-15 12:57:34 +0200

    set movement rule for player and boxes

--- src/main/sk/pavlovsky/sokoban/Game.java
+++ src/main/sk/pavlovsky/sokoban/Game.java
@@ -1,10 +1,13 @@
 package main.sk.pavlovsky.sokoban;
 
 import main.sk.pavlovsky.sokoban.Inputs.ConsoleDirection;
+import main.sk.pavlovsky.sokoban.object.levelActor.Box;
 import main.sk.pavlovsky.sokoban.object.levelObject.Map;
 import main.sk.pavlovsky.sokoban.Inputs.Direction;
+import main.sk.pavlovsky.sokoban.object.levelObject.Wall;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 
 public class Game {
@@ -37,13 +40,35 @@ public class Game {
     private void update(Direction direction) {
         switch (direction) {
             case QUIT :this.running = false;
-            case UP : this.activeMap.getPlayer().move(0, -1); break;
-            case DOWN :  this.activeMap.getPlayer().move(0,1); break;
-            case LEFT : this.activeMap.getPlayer().move(-1,0); break;
-            case RIGHT : this.activeMap.getPlayer().move(1,0); break;
+            case UP : move(0, -1); break;
+            case DOWN :  move(0,1); break;
+            case LEFT : move(-1,0); break;
+            case RIGHT : move(1,0); break;
             default:break; //how to set break with expression
         }
+    }
+    private void move(int dx, int dy){
+        if (dx> 1|| dx<-1 || dy>1 || dy <-1) throw new RuntimeException("Big movement");
+        int x = this.activeMap.getPlayer().getX();
+        int y = this.activeMap.getPlayer().getY();
+        if (x+dx > this.activeMap.getWidth() || y+dy > this.activeMap.getHeight() || this.activeMap.getLevelObject(x+dx, y+dy) instanceof Wall) {
+            return;
+        }
+        Optional<Box> boxOptional = this.activeMap.getBoxes().stream().filter(it -> it.getX() == x+dx && it.getY() == dy+y).findFirst();
+        if (!boxOptional.isPresent()) {
+            this.activeMap.getPlayer().move(dx, dy);
+            return;
+        }
+        if (x+2*dx > this.activeMap.getWidth()||y+2*dy> this.activeMap.getHeight() || this.activeMap.getLevelObject(x+2*dx,y+2*dy) instanceof Wall){
+            return;
+        }
+        Optional<Box> boxOptional1 = this.activeMap.getBoxes().stream().filter(it -> it.getX() == x+2*dx && it.getY() ==y+2*dy).findFirst();
+        if (boxOptional1.isPresent())
+            return;
 
+        Box box = boxOptional.get();
+        box.move(dx,dy);
+        this.activeMap.getPlayer().move(dx,dy);
     }
     private Direction input() {
         return ConsoleDirection.getDirection();
